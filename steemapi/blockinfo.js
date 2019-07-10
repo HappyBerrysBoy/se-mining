2c8c2b9a85e7dabe666fb81b588a74583a34c8ec
happyberrysboy <elytselyts@gmail.com>
2019-07-10 23:23:53 +0900

    'monitoring'

--- steemapi/blockinfo.js
+++ steemapi/blockinfo.js
@@ -9,8 +9,6 @@ const jsonData = {
   lastReadSscBlock: 400005,
 };
 
-
-
 fs.readFile('../logs/block.txt', 'utf8', function(err, data) {
   if (err) console.log(err);
   const json = JSON.parse(data);
@@ -19,55 +17,55 @@ fs.readFile('../logs/block.txt', 'utf8', function(err, data) {
   getBlock(jsonData.lastReadSteemBlock);
 });
 
-  
-
 async function getBlock(lastSteemBlock) {
-    console.log("parameter : " + lastSteemBlock);
-    let blockinfo;
-    const blockno = {lastReadSteemBlock: 34300626};
-    blockno.lastReadSteemBlock = lastSteemBlock;
-    console.log("start : " + blockno.lastReadSteemBlock);
-    while (true) {
-//      console.log(jsonData.lastReadSteemBlock);
-       blockinfo = await steem.api.getBlockAsync(blockno.lastReadSteemBlock);
-        
-        const { timestamp, transactions } = blockinfo;
-      
-        if (transactions.length == 0) return;
-      
-        transactions.forEach(transaction => {
-          const { operations, signatures } = transaction;
-          const action = operations[0][0];
-          const content = operations[0][1];
-          // console.log('action :', action);
-          // console.log('content :', content);
-      
-           if (action === "custom_json") {
-            // console.log('action :', action);
-            // console.log('content :', content);
-      
-            const jsonInfo = JSON.parse(content.json);
-            //console.log(jsonInfo);
-            if (jsonInfo.contractName === "tokens" && jsonInfo.contractAction === "issue") {
-              const payload = jsonInfo.contractPayload;
-              if(payload.symbol === "SCT" && payload.to === "sct.admin"){
-                payload.timestamp = timestamp;
-                payload.block = blockno.lastReadSteemBlock;
+  console.log('parameter : ' + lastSteemBlock);
+  let blockinfo;
+  const blockno = { lastReadSteemBlock: 34300626 };
+  blockno.lastReadSteemBlock = lastSteemBlock;
+  console.log('start : ' + blockno.lastReadSteemBlock);
+  while (true) {
+    //      console.log(jsonData.lastReadSteemBlock);
+    blockinfo = await steem.api.getBlockAsync(blockno.lastReadSteemBlock);
 
-                fs.appendFile('../logs/sct_beneficery.txt', JSON.stringify(payload) + '\n', err => {
-                  if (err) console.log(err);
-                });
-              }
-            } 
+    const { timestamp, transactions } = blockinfo;
+
+    if (transactions.length == 0) return;
+
+    transactions.forEach(transaction => {
+      const { operations, signatures } = transaction;
+      const action = operations[0][0];
+      const content = operations[0][1];
+
+      if (action === 'custom_json') {
+        const jsonInfo = JSON.parse(content.json);
+        //console.log(jsonInfo);
+        if (
+          jsonInfo.contractName === 'tokens' &&
+          jsonInfo.contractAction === 'issue'
+        ) {
+          const payload = jsonInfo.contractPayload;
+          if (payload.symbol === 'SCT' && payload.to === 'sct.admin') {
+            payload.timestamp = timestamp;
+            payload.block = blockno.lastReadSteemBlock;
+            console.log(payload);
+
+            fs.appendFile(
+              '../logs/sct_beneficery.txt',
+              JSON.stringify(payload) + '\n',
+              err => {
+                if (err) console.log(err);
+              },
+            );
           }
-        });
+        }
+      }
+    });
 
-        blockno.lastReadSteemBlock += 1;
-  
-        fs.writeFile('../logs/block.txt', JSON.stringify(blockno), err => {
-          if (err) console.log(err);
-          //console.log('The file has been saved!');
-        });
-    }
+    blockno.lastReadSteemBlock += 1;
+
+    fs.writeFile('../logs/block.txt', JSON.stringify(blockno), err => {
+      if (err) console.log(err);
+      //console.log('The file has been saved!');
+    });
+  }
 }
-
