11fbab21cca872ffd5398919adbff7cc89af5acd
happyberrysboy <elytselyts@gmail.com>
2019-07-13 09:44:18 +0900

    'bug_fix'

--- steemapi/blockinfo.js
+++ steemapi/blockinfo.js
@@ -1,16 +1,16 @@
-const steem = require("steem");
-const config = require("../config.json");
+const steem = require('steem');
+const config = require('../config.json');
 // const { createLogger, format, transports } = require("winston");
 //const log = require('log-to-file');
-const fs = require("fs");
-const key = require("../key.json");
+const fs = require('fs');
+const key = require('../key.json');
 
 const jsonData = {
   lastReadSteemBlock: 34543118,
-  lastReadSscBlock: 400005
+  lastReadSscBlock: 400005,
 };
 
-fs.readFile("../config/blockConfig.ini", "utf8", function(err, data) {
+fs.readFile('./config/blockConfig.ini', 'utf8', function(err, data) {
   if (err) console.log(err);
   const json = JSON.parse(data);
   console.log(json.lastReadSteemBlock);
@@ -19,11 +19,11 @@ fs.readFile("../config/blockConfig.ini", "utf8", function(err, data) {
 });
 
 async function getBlock(lastSteemBlock) {
-  console.log("parameter : " + lastSteemBlock);
+  console.log('parameter : ' + lastSteemBlock);
   let blockinfo;
   const blockno = { lastReadSteemBlock: 34300626 };
   blockno.lastReadSteemBlock = lastSteemBlock;
-  console.log("start : " + blockno.lastReadSteemBlock);
+  console.log('start : ' + blockno.lastReadSteemBlock);
 
   let retryCnt = 0;
 
@@ -31,9 +31,9 @@ async function getBlock(lastSteemBlock) {
     const date = new Date();
     // AWS 시간이 UTC 기준이라 국내보다 9시간 늦음 그래서 강제로 9시간 빠르게 돌림
     date.setHours(date.getHours() + 9);
-    const year = date.getFullYear() + "";
-    const month = (date.getMonth() + 1 + "").padStart(2, "0");
-    const day = (date.getDate() + "").padStart(2, "0");
+    const year = date.getFullYear() + '';
+    const month = (date.getMonth() + 1 + '').padStart(2, '0');
+    const day = (date.getDate() + '').padStart(2, '0');
     const dateString = `${year}-${month}-${day}`;
 
     if (blockno.lastReadSteemBlock % 100 == 0) {
@@ -57,22 +57,22 @@ async function getBlock(lastSteemBlock) {
       console.log(e);
       console.log(`const { timestamp = null, transactions } = blockinfo error`);
       fs.appendFile(
-        "../logs/exceptions(" + dateString + ").txt",
-        JSON.stringify(blockinfo) + "\n",
+        './logs/exceptions(' + dateString + ').txt',
+        JSON.stringify(blockinfo) + '\n',
         err => {
           if (err) console.log(err);
-        }
+        },
       );
 
       // 해당 블럭 읽기 재시도
       retryCnt++;
       if (retryCnt > 3) {
         fs.appendFile(
-          "../logs/exceptions(" + dateString + ").txt",
-          "retry count over\n",
+          './logs/exceptions(' + dateString + ').txt',
+          'retry count over\n',
           err => {
             if (err) console.log(err);
-          }
+          },
         );
         break;
       } else {
@@ -89,45 +89,57 @@ async function getBlock(lastSteemBlock) {
       const content = operations[0][1];
       content.blocknumber = blockno.lastReadSteemBlock;
 
-      if (action === "custom_json") {
-        const jsonInfo = JSON.parse(content.json);
-        jsonInfo.timestamp = timestamp;
-        jsonInfo.blocknumber = blockno.lastReadSteemBlock;
+      if (action === 'custom_json') {
+        try {
+          const jsonInfo = JSON.parse(content.json);
+          jsonInfo.timestamp = timestamp;
+          jsonInfo.blocknumber = blockno.lastReadSteemBlock;
 
-        // 용량 많이 잡아먹어서 하루치만
-        // fs.appendFile(
-        //   '../logs/customjson(' + dateString + ').txt',
-        //   JSON.stringify(content) + '\n',
-        //   err => {
-        //     if (err) console.log(err);
-        //   },
-        // );
+          // 용량 많이 잡아먹어서 하루치만
+          // fs.appendFile(
+          //   './logs/customjson(' + dateString + ').txt',
+          //   JSON.stringify(content) + '\n',
+          //   err => {
+          //     if (err) console.log(err);
+          //   },
+          // );
 
-        if (
-          content.id === config.customJsonList.mining &&
-          jsonInfo.type === config.customJsonList.mining_type
-        ) {
-          const winner = jsonInfo.winner;
-          const amount = jsonInfo.claim_token_amount;
-          const miningPower = jsonInfo.staked_mining_power;
-          const symbol = jsonInfo.symbol;
-          const blockNum = jsonInfo.block_num;
+          if (
+            content.id === config.customJsonList.mining &&
+            jsonInfo.type === config.customJsonList.mining_type
+          ) {
+            const winner = jsonInfo.winner;
+            const amount = jsonInfo.claim_token_amount;
+            const miningPower = jsonInfo.staked_mining_power;
+            const symbol = jsonInfo.symbol;
+            const blockNum = jsonInfo.block_num;
 
-          console.log("content :", jsonInfo);
+            console.log('content :', jsonInfo);
 
+            fs.appendFile(
+              './logs/mining(' + dateString + ').txt',
+              JSON.stringify(jsonInfo) + '\n',
+              err => {
+                if (err) console.log(err);
+              },
+            );
+            // {"service":"SE_MINING","content":"key:id, content:scot_claim","level":"info","message":"info","timestamp":"2019-06-25 01:42:46"}
+            // {"service":"SE_MINING","content":"key:json, content:{\"symbol\":\"PAL\",\"type\":\"mining\",\"N\":9,\"staked_mining_power\":2313.0000000000005,\"winner\":[\"bitcoinflood\",\"jongolson\",\"michealb\",\"nuthman\",\"aggroed\",\"dylanhobalart\",\"dylanhobalart\",\"videosteemit\",\"steinreich\"],\"claim_token_amount\":2.067,\"trx_id\":\"4654e524c287b4354981587aea3a62f133da8648\",\"block_num\":34084567,\"N_accounts\":166}","level":"info","message":"info","timestamp":"2019-06-25 01:42:46"}
+          } else {
+          }
+        } catch (e) {
+          console.log(e);
           fs.appendFile(
-            "../logs/mining(" + dateString + ").txt",
-            JSON.stringify(jsonInfo) + "\n",
+            './logs/exceptions(' + dateString + ').txt',
+            'retry count over\n',
             err => {
               if (err) console.log(err);
-            }
+            },
           );
-          // {"service":"SE_MINING","content":"key:id, content:scot_claim","level":"info","message":"info","timestamp":"2019-06-25 01:42:46"}
-          // {"service":"SE_MINING","content":"key:json, content:{\"symbol\":\"PAL\",\"type\":\"mining\",\"N\":9,\"staked_mining_power\":2313.0000000000005,\"winner\":[\"bitcoinflood\",\"jongolson\",\"michealb\",\"nuthman\",\"aggroed\",\"dylanhobalart\",\"dylanhobalart\",\"videosteemit\",\"steinreich\"],\"claim_token_amount\":2.067,\"trx_id\":\"4654e524c287b4354981587aea3a62f133da8648\",\"block_num\":34084567,\"N_accounts\":166}","level":"info","message":"info","timestamp":"2019-06-25 01:42:46"}
-        } else {
+          return;
         }
       } else if (
-        action === "comment" &&
+        action === 'comment' &&
         content.body.indexOf(config.pickTag) > -1
       ) {
         try {
@@ -138,8 +150,8 @@ async function getBlock(lastSteemBlock) {
           let pickCnt = parseInt(
             content.body
               .split(config.pickTag)[1]
-              .split("(")[0]
-              .trim()
+              .split('(')[0]
+              .trim(),
           );
 
           // 몇명 뽑는지 입력 안하면 1명으로 설정
@@ -148,9 +160,9 @@ async function getBlock(lastSteemBlock) {
           }
 
           const list = hdata[0]
-            .replace(/\(/g, "")
-            .replace(/\)/g, "")
-            .split(",");
+            .replace(/\(/g, '')
+            .replace(/\)/g, '')
+            .split(',');
 
           let pickAccount = [];
           let pickedNum = [];
@@ -160,10 +172,10 @@ async function getBlock(lastSteemBlock) {
           let numOfTotalVal = 0;
 
           list.forEach(a => {
-            accountList.push(a.split(":")[0].replace(/ /g, ""));
-            if (a.indexOf(":") > -1) {
-              valList.push(parseFloat(a.split(":")[1].replace(/ /g, "")));
-              numOfTotalVal += parseFloat(a.split(":")[1].replace(/ /g, ""));
+            accountList.push(a.split(':')[0].replace(/ /g, ''));
+            if (a.indexOf(':') > -1) {
+              valList.push(parseFloat(a.split(':')[1].replace(/ /g, '')));
+              numOfTotalVal += parseFloat(a.split(':')[1].replace(/ /g, ''));
             } else {
               valList.push(1);
               numOfTotalVal += 1;
@@ -171,7 +183,7 @@ async function getBlock(lastSteemBlock) {
           });
 
           if (!numOfTotalVal) {
-            throw "Nan Error Block:" + content.blocknumber;
+            throw 'Nan Error Block:' + content.blocknumber;
           }
 
           if (list.length < pickCnt) {
@@ -181,7 +193,7 @@ async function getBlock(lastSteemBlock) {
           // console.log(accountList, valList, numOfTotalVal);
 
           let body = `##### Happy Pick Result!!(Pick ${pickCnt} account${
-            pickCnt < 2 ? "" : "s"
+            pickCnt < 2 ? '' : 's'
           })\n\nPlay${hdata}\n\nRanking..!!\n`;
 
           while (pickAccount.length < pickCnt) {
@@ -214,27 +226,27 @@ async function getBlock(lastSteemBlock) {
             key.happyberrysboy_posting,
             content.author,
             content.permlink,
-            "happyberrysboy",
+            'happyberrysboy',
             steem.formatter
               .commentPermlink(content.author, content.permlink)
-              .replace(/\./g, "")
+              .replace(/\./g, '')
               .substring(0, 16) + Math.floor(Math.random() * 10000),
-            "",
+            '',
             body,
             content.json_metadata,
             function(err, result) {
               console.log(err, result);
-            }
+            },
           );
 
           const logJson = { content: content, result: body };
 
           fs.appendFile(
-            "../logs/happypick(" + dateString + ").txt",
-            JSON.stringify(logJson) + "\n",
+            './logs/happypick(' + dateString + ').txt',
+            JSON.stringify(logJson) + '\n',
             err => {
               if (err) console.log(err);
-            }
+            },
           );
         } catch (e) {
           console.log(e);
@@ -242,14 +254,14 @@ async function getBlock(lastSteemBlock) {
             key.happyberrysboy_posting,
             content.author,
             content.permlink,
-            "happyberrysboy",
+            'happyberrysboy',
             steem.formatter.commentPermlink(content.author, content.permlink),
-            "",
-            "입력하는데 뭔가 실수 하셨는데예?",
+            '',
+            '입력하는데 뭔가 실수 하셨는데예?',
             content.json_metadata,
             function(err, result) {
               console.log(err, result);
-            }
+            },
           );
         }
       }
@@ -257,7 +269,7 @@ async function getBlock(lastSteemBlock) {
 
     blockno.lastReadSteemBlock += 1;
 
-    fs.writeFile("../config/blockConfig.ini", JSON.stringify(blockno), err => {
+    fs.writeFile('./config/blockConfig.ini', JSON.stringify(blockno), err => {
       if (err) console.log(err);
       //console.log('The file has been saved!');
     });
