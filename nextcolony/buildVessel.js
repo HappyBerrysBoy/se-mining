00f8efbb03a8087e222399e01e4d5f660a04162b
HappyBerrysBoy <elytselyts@gmail.com>
2019-07-28 02:02:03 +0900

    build_vessel

--- nextcolony/buildVessel.js
+++ nextcolony/buildVessel.js
@@ -1,17 +1,17 @@
-const steem = require('steem');
-const key = require('../key.json');
+const steem = require("steem");
+const key = require("../key.json");
 
 // 성공
 // Nextcolony Build Ship
 steem.broadcast.customJson(
   key.happyberrysboy_posting, // posting key
   [],
-  ['happyberrysboy'], // account
-  'nextcolony', // 'nextcolony'
-  `{"username":"happyberrysboy","type":"buildship","command":{"tr_var1":"P-ZA01QNQO29C","tr_var2":"explorership"}}`, // content json stringfy
+  ["happyberrysboy"], // account
+  "nextcolony", // 'nextcolony'
+  `{"username":"happyberrysboy","type":"buildship","command":{"tr_var1":"P-ZO75DZDVRUO","tr_var2":"explorership"}}`, // content json stringfy
   function(err, result) {
     console.log(err, result);
-  },
+  }
 );
 
 // Nextcolony Explore
