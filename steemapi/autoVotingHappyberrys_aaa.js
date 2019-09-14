632c2f8cf386bdb4f7f14ac9936c0c766986b042
happyberrysboy <elytselyts@gmail.com>
2019-09-15 02:34:01 +0900

    ..

--- steemapi/autoVotingHappyberrys_aaa.js
+++ steemapi/autoVotingHappyberrys_aaa.js
@@ -45,8 +45,7 @@ cron.schedule("*/20 * * * * *", function() {
     feedData.forEach(async content => {
       const diffTime =
         (new Date().getTime() - new Date(content.created).getTime()) /
-          (1000 * 60) -
-        9 * 60;
+        (1000 * 60);
 
       console.log(`diff time:${diffTime}`);
       if (diffTime < 200 && diffTime >= 15) {
