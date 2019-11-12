45c21b7c88539db6eaccd0d6316a245ea20613d0
happyberrysboy <elytselyts@gmail.com>
2019-11-13 02:47:19 +0900

    ..

--- nextcolony/steemservice.js
+++ nextcolony/steemservice.js
@@ -18,7 +18,7 @@ const explorePlanetArray = [
   {
     name: "A",
     id: "P-ZLRD1LQB8N4",
-    exploreCnt: 10,
+    exploreCnt: 5,
     explorerDirection: {
       xminus: true,
       xplus: false,
@@ -29,7 +29,7 @@ const explorePlanetArray = [
   {
     name: "B",
     id: "P-ZTUAR18RZ9C",
-    exploreCnt: 20,
+    exploreCnt: 5,
     explorerDirection: {
       xminus: true,
       xplus: false,
@@ -68,13 +68,13 @@ const skillUpArray = [
 
 let attackIdx = 0;
 const attackList = [
-  { id: "P-Z4F8YZE0XJ4", x: -543, y: 122 },
+  { id: "P-Z3TXPUXUCYO", x: -544, y: 143 },
+  { id: "P-ZNYQLE6J81C", x: -543, y: 126 },
+  { id: "P-ZAIUWOOL62O", x: -545, y: 115 },
   { id: "P-ZTY87WKC52O", x: -546, y: 117 },
   { id: "P-Z2DEL2ENL34", x: -528, y: 110 },
   { id: "P-Z39X5A042K0", x: -531, y: 106 },
-  { id: "P-ZAIUWOOL62O", x: -545, y: 115 },
-  { id: "P-Z3TXPUXUCYO", x: -544, y: 143 },
-  { id: "P-ZNYQLE6J81C", x: -543, y: 126 }
+  { id: "P-Z4F8YZE0XJ4", x: -543, y: 122 }
 ];
 
 let buildArray = [];
