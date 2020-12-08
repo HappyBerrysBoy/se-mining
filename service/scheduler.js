5f701b41072b085d6885ee94db78431aa38b604b
happyberrysboy <elytselyts@gmail.com>
2020-12-08 21:42:35 +0900

    change scheduler time

--- service/scheduler.js
+++ service/scheduler.js
@@ -3,11 +3,11 @@ const maiPosting = require("../posting/mai.js");
 const maiPostingFee = require("../posting/mai_postingfee.js");
 
 // mai님 포스팅
-schedule.scheduleJob("3 10 * * *", async function () {
+schedule.scheduleJob("0 13 * * *", async function () {
   console.log("================= start maikuraki posting ====================");
   maiPosting();
 });
 
-schedule.scheduleJob("5 10 * * *", async function () {
+schedule.scheduleJob("1 13 * * *", async function () {
   maiPostingFee();
 });
